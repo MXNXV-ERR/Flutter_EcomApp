0f274eb843e0200253c193293a1c78001be35149
MXNXV-ERR <MXNXV-ERR@users.noreply.github.com>
2022-08-16 12:03:10 +0530

    product page fitsr biuldd

--- lib/pages/productdetails.dart
+++ lib/pages/productdetails.dart
@@ -40,18 +40,105 @@ class _Product_DetailsState extends State<Product_Details> {
       body: ListView(
         children: [
           Container(
+            height: 300.0,
             child: GridTile(
               child: Container(
-                color: Colors.white,
+                //color: Colors.white,
                 child: Image.asset(widget.pic),
               ),
               footer: Container(
-                  color: Colors.white,
+                  color: Colors.white70,
                   child: ListTile(
-                    leading: null,
+                    leading: Text(
+                      widget.name,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 16.0),
+                    ),
+                    title: Row(
+                      children: [
+                        Expanded(
+                            child: Text("\$${widget.oprice}",
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  decoration: TextDecoration.lineThrough,
+                                ))),
+                        Expanded(
+                            child: Text(
+                          "\$${widget.price}",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.green),
+                        ))
+                      ],
+                    ),
                   )),
             ),
-          )
+          ),
+          Row(
+            children: [
+              Expanded(
+                  child: MaterialButton(
+                elevation: 2.0,
+                onPressed: null,
+                color: Colors.white,
+                textColor: Colors.grey,
+                child: Row(
+                  children: [
+                    Expanded(child: Text("Size")),
+                    Expanded(child: Icon(Icons.arrow_drop_down))
+                  ],
+                ),
+              )),
+              Expanded(
+                  child: MaterialButton(
+                elevation: 2.0,
+                onPressed: null,
+                color: Colors.white,
+                textColor: Colors.grey,
+                child: Row(
+                  children: [
+                    Expanded(child: Text("Color")),
+                    Expanded(child: Icon(Icons.arrow_drop_down))
+                  ],
+                ),
+              )),
+              Expanded(
+                  child: MaterialButton(
+                elevation: 2.0,
+                onPressed: null,
+                color: Colors.white,
+                textColor: Colors.grey,
+                child: Row(
+                  children: [
+                    Expanded(child: Text("Qty")),
+                    Expanded(child: Icon(Icons.arrow_drop_down))
+                  ],
+                ),
+              ))
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                  child: MaterialButton(
+                      elevation: 2.0,
+                      onPressed: () {},
+                      color: Colors.blueGrey,
+                      textColor: Colors.white,
+                      child: new Text("Buy Now"))),
+              const IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.add_shopping_cart,
+                    color: Colors.blueGrey,
+                  )),
+              const IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.favorite_border,
+                    color: Colors.blueGrey,
+                  ))
+            ],
+          ),
         ],
       ),
     );
