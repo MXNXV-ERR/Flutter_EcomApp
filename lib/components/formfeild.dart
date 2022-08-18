511c3f61b0764f09e6341c68de74f181855fd8c7
MXNXV-ERR <MXNXV-ERR@users.noreply.github.com>
2022-08-18 20:29:43 +0530

    snackbar depcreated

---
+++ lib/components/formfeild.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class TxtFrmFld extends StatefulWidget {
+  final String? Function(String?) validator;
+  final String label;
+  final Function onChanged;
+
+  TxtFrmFld({
+    Key? key,
+    required this.onChanged,
+    required this.validator,
+    required this.label,
+  }) : super(key: key);
+
+  @override
+  State<TxtFrmFld> createState() => _TxtFrmFldState();
+}
+
+class _TxtFrmFldState extends State<TxtFrmFld> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 275,
+      child: TextFormField(
+        validator: widget.validator,
+        decoration: InputDecoration(
+          label: Text(widget.label),
+          floatingLabelBehavior: FloatingLabelBehavior.auto,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+        ),
+      ),
+    );
+  }
+}
+
+class PwdFrmFld extends StatefulWidget {
+  bool obsecureTxt;
+  final String? Function(String?) validator;
+  final String label;
+  final void Function() onTap;
+  final Function onChanged;
+  PwdFrmFld(
+      {Key? key,
+      required this.onChanged,
+      required this.obsecureTxt,
+      required this.validator,
+      required this.label,
+      required this.onTap})
+      : super(key: key);
+
+  @override
+  State<PwdFrmFld> createState() => _PwdFrmFldState();
+}
+
+class _PwdFrmFldState extends State<PwdFrmFld> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 275,
+      child: TextFormField(
+        validator: widget.validator,
+        obscureText: widget.obsecureTxt,
+        decoration: InputDecoration(
+          //contentPadding: const EdgeInsets.only(top: 20.0),
+          label: const Text('Password'),
+          floatingLabelBehavior: FloatingLabelBehavior.auto,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+          suffixIcon: GestureDetector(
+            onTap: () {
+              setState(() {
+                widget.obsecureTxt = !widget.obsecureTxt;
+              });
+            },
+            child: Icon(
+              widget.obsecureTxt == true
+                  ? Icons.visibility
+                  : Icons.visibility_off,
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -6,6 +6,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 //import 'package:google_sign_in/google_sign_in.dart';
 //import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerce_app/components/auth.dart';
+import 'package:ecommerce_app/components/formfeild.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -15,23 +16,129 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  //late final ScaffoldMessengerState? _scaffold = _scaffoldKey.currentState;
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+
+  String email = "";
+  String password = "";
+  String p =
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+  late RegExp regExp = RegExp(p);
+  bool obsecureTxt = false;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //scaffoldMessengerKey: _scaffoldKey,
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Login"),
-        ),
+        key: _scaffoldKey,
+        // appBar: AppBar(
+        //   title: const Text("Login"),
+        // ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              //Email - password login
-              const Text("Sign-in"),
-              Divider(),
+              //===============================Logo Holder=====================
               Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15.0),
+                padding: EdgeInsets.only(bottom: 75.0),
+                child: SizedBox(
+                  height: 120.0,
+                  width: 120.0,
+                  child: Container(
+                    color: Colors.amber,
+                    child: Center(child: Text("Logo holder")),
+                  ),
+                ),
+              ),
+              //===============Email - password login====================
+              Form(
+                //sdfgautovalidateMode: AutovalidateMode.onUserInteraction,
+                //autovalidateMode: AutovalidateMode.always,
+                key: _formkey,
+                child: Column(//direction: Axis.vertical,
+                    children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 5.0),
+                    child: TxtFrmFld(
+                        onChanged: (value) {
+                          validation();
+                          email = value;
+                          setState(() {
+                            validation();
+                            email = value.toString().trim();
+                            print("this" + email);
+                          });
+                        },
+                        validator: (value) {
+                          if (value == "") {
+                            return "Please fill the email";
+                          } else if (!regExp.hasMatch(value!)) {
+                            return "Email is Invalid";
+                          }
+                          setState(() {
+                            email = value.toString().trim();
+                          });
+                          return null;
+                        },
+                        label: "Email"),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 5.0),
+                    child: PwdFrmFld(
+                      onChanged: (value) {
+                        setState(() {
+                          validation();
+                          password = value;
+                        });
+                      },
+                      obsecureTxt: true,
+                      validator: (value) {
+                        if (value == "") {
+                          return 'Please enter the password';
+                        } else if (value!.length < 8) {
+                          return "Password too short";
+                        }
+                        setState(() {
+                          password = value.toString().trim();
+                        });
+                        return null;
+                      },
+                      label: 'Password',
+                      onTap: () {
+                        setState(() {
+                          obsecureTxt = !obsecureTxt;
+                        });
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: Container(
+                      width: 275,
+                      height: 65,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 15.0),
+                        child: ElevatedButton(
+                            onPressed: validation,
+                            child: Text("Login"),
+                            style: ButtonStyle(
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12.0),
+                            )))),
+                      ),
+                    ),
+                  )
+                ]),
+              ),
+
+              const Divider(),
+
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0),
                 child: Text(
                   "Other Sign-in Options",
                   style: TextStyle(color: Colors.black45),
@@ -58,6 +165,30 @@ class _LoginState extends State<Login> {
         ),
       ),
     );
+  }
+
+  Future<void> validation() async {
+    FocusManager.instance.primaryFocus?.unfocus();
+    final FormState? _form = _formkey.currentState;
+    if (_form!.validate()) {
+      try {
+        UserCredential result = await FirebaseAuth.instance
+            .signInWithEmailAndPassword(email: email, password: password);
+        print(result.user!.uid);
+      } on FirebaseException catch (e) {
+        print(e);
+
+        _showSnackBar(e.message.toString());
+      }
+    } else {
+      print("No");
+
+      _showSnackBar("Please wait... Processing data");
+    }
+  }
+
+  void _showSnackBar(String content) {
+    _scaffoldKey.currentState?.showSnackBar(SnackBar(content: Text(content)));
   }
 }
 
